292ce113a70e0f452da927445737a2b83988afaf
u1810291 <otabekbutcher@gmail.com>
2023-02-27 10:11:33 +0500

    feat: landing

---
+++ components/Slider/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react' // import from 'keen-slider/react.es' for to get an ES module
+
+export default function Slider() {
+  const [sliderRef, instanceRef] = useKeenSlider(
+    {
+      slideChanged() {
+        console.log('slide changed')
+      },
+    },
+    []
+    )
+
+  return (
+    <div ref={sliderRef} className='keen-slider h-1/6'>
+      {[...(new Array(10))].map((_, index) =>
+        <div key={index} className='keen-slider__slide bg-orange-600'>{index + 1}</div>
+      )}
+    </div>
+  )
+}
---
+++ pages/mortgage/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Mortgage() {
+  return (
+    <div>Mortgage</div>
+  )
+}
